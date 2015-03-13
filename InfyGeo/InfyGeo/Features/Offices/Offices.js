20fe2142f1d77eac1e1872608dc51997ab53f0d6
test user <test user @gmail.com>
2015-03-12 22:13:20 -0400

    Adding project structure

---
+++ InfyGeo/InfyGeo/Features/Offices/Offices.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=Offices.js.map
---
+++ InfyGeo/InfyGeo/app.js
@@ -0,0 +1,14 @@
+﻿
+angular.module('InfyGeo', [])
+  .controller('HomeController', ['$scope', function ($scope) {
+
+      $scope.Title = "InfoGeo";
+      $scope.Description = "Geographical features for Infosys employees";
+
+      $scope.goToPeople = function () {
+
+          alert('Clicked!');
+          //$location.path('/Features/People/People.html');
+      };
+
+  }]);
