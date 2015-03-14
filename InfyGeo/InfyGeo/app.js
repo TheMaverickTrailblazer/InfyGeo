bb6cad73481065255cec63708b9033ff3c731be7
test user <test user @gmail.com>
2015-03-14 12:52:16 -0400

    Solving merge issue

--- InfyGeo/InfyGeo/app.js
+++ InfyGeo/InfyGeo/app.js
@@ -34,15 +34,9 @@
                    $scope.People = data;
                });
 
-           }]).controller('PeopleController', ['$scope', function ($scope) {
-               $scope.People = [
-                 { Name: 'Karthik Jambulingam', City: "Plainsboro", Distance: '10', Email: "karthik_j02@infosys.com" },
-                 { Name: 'Amit Desai', City: "Hopewell", Distance: '30', Email: "Amit.Desai@infosys.com" },
-                 { Name: 'Mahalingam Kumaresan', City: "New York", Distance: '50', Email: "Mahalingam_k03@infosys.com" },
-                 { Name: 'Abhilash Nair', City: "Jersey City", Distance: '40', Email: "Abhilash.Nair@infosys.com" }
-               ];
-
-               $scope.Title = "Nearby People";
+               $http.get('Features/People/CompanionService.txt').success(function (data) {
+                   $scope.Comapanions = data;
+               });
            }]).controller('KeyInfoController', ['$scope', '$http', function ($scope, $http) {
                var location = $scope.Location;
                var url = "http://dev.api.ean.com/ean-services/rs/hotel/v3/list?cid=38998&apiKey=53s3gvvk29x99zvt54zmnnzh&minorRev=28&customerUserAgent=WEB_SITE&customerIpAddress=73.193.252.64&locale=en_US&currencyCode=USD&city=" + location + "&stateProvinceCode=NJ&countryCode=US";
@@ -58,5 +52,4 @@
                                      // or server returns response with an error status.
                                      alert('an error occured');
                                  });
-           }])
-;
+           }]);
