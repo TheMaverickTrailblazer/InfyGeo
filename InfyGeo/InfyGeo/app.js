a45931aa4a8f26cb41ea203e9bab25b8d7399cca
test user <test user @gmail.com>
2015-03-15 10:58:05 -0400

    Included people locate feature

--- InfyGeo/InfyGeo/app.js
+++ InfyGeo/InfyGeo/app.js
@@ -50,9 +50,9 @@
                    return person.Distance < 50 || person.Location == $scope.Location;
                };
 
-               $scope.showMap = function () {
-                   alert($scope.Location);
-                   initialize($scope.Location, null, null, '', '',true);
+               $scope.showMap = function (city) {
+                  // alert(city);
+                   initialize(city, null, null, '', '', true);
                };
 
            }]).controller('KeyInfoController', ['$scope', '$http', function ($scope, $http) {
